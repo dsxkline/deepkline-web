f0e898f9690228b6ae1d6531421a7170e5e4a729
deepkline <dangfm@qq.com>
2025-05-26 11:46:38 +0800

    pwa 配置

--- config/pwa.config.ts
+++ config/pwa.config.ts
@@ -31,29 +31,32 @@ export default {
 		]
 	},
 	workbox: {
-      runtimeCaching: [
-        {
-          urlPattern: '.*', // 匹配所有 URL
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'all-cache',
-            expiration: {
-            //   maxEntries: 200, // 最多缓存 200 个条目
-              maxAgeSeconds: 60 * 60 * 24 * 7, // 缓存 7 天
-            },
-            cacheableResponse: {
-              statuses: [0, 200], // 只缓存这些响应码的资源
-            },
-          },
-        },
-      ],
-    },
+		offline: false,
+		runtimeCaching: [
+			{
+				urlPattern: '.*', // 匹配所有 URL
+				handler: 'StaleWhileRevalidate',
+				method: 'GET',
+				options: {
+					cacheName: 'all-cache',
+					expiration: {
+						//   maxEntries: 200, // 最多缓存 200 个条目
+						maxAgeSeconds: 60 * 60 * 24 * 7 // 缓存 7 天
+					},
+					networkTimeoutSeconds: 30,
+					cacheableResponse: {
+						statuses: [0, 200] // 只缓存这些响应码的资源
+					}
+				}
+			}
+		]
+	},
 	meta: {
 		theme_color: '#1e0b2c', // 确保 meta 标签也设置
 		appleMobileWebAppCapable: true,
 		appleMobileWebAppStatusBarStyle: 'default',
 		mobileAppIOS: true,
 		appleStatusBarStyle: 'black-translucent',
-		appleIcon: '/images/pwa/apple-touch-icon.png', // 指定 iOS icon
+		appleIcon: '/images/pwa/apple-touch-icon.png' // 指定 iOS icon
 	}
 }
