8be9c52478822c9e8e0758b3a64514aa3e1bbc43
deepkline <dangfm@qq.com>
2025-05-29 14:51:59 +0800

    push 优化

--- components/app/PushView.vue
+++ components/app/PushView.vue
@@ -246,12 +246,12 @@
 				width: 100%;
 				height: 100%;
 				content: '';
-				z-index: 0;
+				z-index: -1;
 				opacity: 0.15;
 			}
 		}
 		.drawer-bg {
-			background: rgb(--color-bg-mask);
+			background: rgb(var(--color-bg-mask));
 			opacity: 0;
 			transition: opacity var(--el-transition-duration);
 			z-index: -1;
