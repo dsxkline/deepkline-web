d760a73306e9853843c297a0e1cab364a5371d0e
deepkline <dangfm@qq.com>
2025-06-15 19:20:55 +0800

    热门板块

--- components/datas/MarketCategories.vue
+++ components/datas/MarketCategories.vue
@@ -10,9 +10,9 @@
 		<ul class="flex w-max">
 			<template v-for="item in categories">
 				<li class="p-2 rounded-md bg-[--transparent02] w-[calc(var(--body-width)/2)] mr-2">
-					<h4 class="text-sm">Liquid Staking</h4>
+					<h4 class="text-sm font-bold">Liquid Staking</h4>
 					<div class="text-sm flex flex-col py-3">
-						<b class="text-green">+1.70%</b>
+						<b class="text-green text-base font-bold">+1.70%</b>
 						<span class="text-xs text-grey">24小时涨跌</span>
 					</div>
 					<div class="text-xs flex"><span>RPL</span><span class="text-green">+8.68%</span></div>
