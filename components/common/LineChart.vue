8df8dfa89f6cb40416f5d9bf1935ed3a07973a95
deepkline <dangfm@qq.com>
2025-06-15 21:58:59 +0800

    热门板块图表

--- components/common/LineChart.vue
+++ components/common/LineChart.vue
@@ -1,14 +1,12 @@
-<!-- 杠杆多空比 -->
 <script setup lang="ts">
 	import { onMounted, ref } from 'vue'
 	import * as echarts from 'echarts'
 	import { ComposFetch } from '@/fetch'
 	import { Period, type Instruments } from '@/fetch/okx/okx.type.d'
-	import moment from 'moment'
 	import { useSymbolStore } from '~/store/symbol'
 	import { useStore } from '~/store'
 	const chart = ref(null)
-	const period = ref('1H')
+	const period = ref('1D')
 	const loading = ref(true)
 	const error = ref('')
 	const props = defineProps<{
@@ -16,12 +14,12 @@
 	}>()
 	const symbolObj = computed<Instruments>(() => useSymbolStore().symbols[props.symbol])
 	let echart: echarts.ECharts | null
-	let xAxisData: string[] | null = []
 	let seriesData: number[] | null = []
 	// 在组件顶部声明 resizeObserver
 	let resizeObserver: ResizeObserver | null = null
 	const containerRef = ref(null)
 	const width = ref<number>(0)
+	const height = ref<number>(0)
 	const disabled = ref(false)
 	const option = {
 		legend: {
@@ -30,62 +28,19 @@
 			itemHeight: 6,
 			left: 0
 		},
-		tooltip: {
-			trigger: 'axis'
-			// textStyle: {
-			// 	fontSize: 12
-			// }
-			// formatter: function (params: any[]) {
-			// 	// console.log('params',params);
-			// 	const item = params[0]
-			// 	const d = moment(parseFloat(item['axisValue'])).format('MM/DD HH:mm')
-			// 	return `${d}</br>${item.value}`
-			// }
-		},
 		grid: {
-			containLabel: true,
-			top: '10', // 图表容器的上边距
-			bottom: '10', // 图表容器的下边距
+			containLabel: false,
+			top: '0', // 图表容器的上边距
+			bottom: '0', // 图表容器的下边距
 			left: '0', // 图表容器的左边距
 			right: '0' // 图表容器的右边距
 		},
 		xAxis: {
 			type: 'category',
-			boundaryGap: false,
-			data: xAxisData,
-			axisLabel: {
-				show: true,
-				interval: function (index: number, value: string) {
-					// 显示固定三个刻度
-					const total = xAxisData?.length || 0 // 总共数据长度
-					const showIndex = [0, Math.floor(total / 2), total - 1]
-					return showIndex.includes(index)
-				},
-				rich: {
-					l: {
-						padding: [0, -70, 0, 0] // 偏移量可根据label文字长度计算
-					},
-					r: {
-						padding: [0, 70, 0, 0] // 偏移量可根据label文字长度计算
-					}
-				},
-				formatter: function (value: string, index: number) {
-					if (index === 0) {
-						return `{l|${value}}`
-					}
-					if (xAxisData && index === xAxisData.length - 1) {
-						return `{r|${value}}`
-					}
-					return value
-				}
-			}
+			boundaryGap: false
 		},
 		yAxis: {
-			type: 'value',
-			boundaryGap: ['100%', '100%'],
-			// min:"dataMin",
-			scale: true,
-			data: seriesData
+			type: 'value'
 		},
 		series: [
 			{
@@ -134,20 +89,13 @@
 				loading.value = false
 				disabled.value = false
 				if (res?.code == 0) {
-					xAxisData = []
 					seriesData = []
-					option.xAxis.data = xAxisData
-					option.series[0].data = seriesData
-
 					res.data.slice(0, 50).forEach(([ts, longShortAccountRatio]: any) => {
-						if (p == Period.M5) xAxisData && xAxisData.push(moment(parseFloat(ts)).format('MM/DD HH:mm'))
-						if (p == Period.H1) xAxisData && xAxisData.push(moment(parseFloat(ts)).format('MM/DD HH:mm'))
-						if (p == Period.D1) xAxisData && xAxisData.push(moment(parseFloat(ts)).format('YYYY/MM/DD'))
 						seriesData && seriesData.push(longShortAccountRatio)
 					})
-					option.xAxis.data = xAxisData.reverse()
+
 					option.series[0].data = seriesData.reverse()
-					option.yAxis.data = seriesData.reverse()
+
 					createEchart()
 					// console.log(xAxisData,seriesData);
 				} else {
@@ -185,10 +133,9 @@
 		// 获取父级的宽度和内边距paddingLeft
 		if (containerRef.value) {
 			const parentElement = containerRef.value as HTMLElement
-			const parentWidth = (parentElement.parentNode as HTMLElement).clientWidth
-			const parentPaddingLeft = (parentElement.parentNode as HTMLElement).getBoundingClientRect().left - parentElement.getBoundingClientRect().left
-			width.value = parentWidth - 2 * Math.abs(parentPaddingLeft)
-			echart && echart.resize({ width: width.value })
+			width.value = parentElement.offsetWidth
+			height.value = parentElement.offsetHeight
+			echart && echart.resize({ width: width.value,height:height.value })
 		}
 	}
 
@@ -205,7 +152,7 @@
 			// 监听父级元素宽度变化
 			const parentElement = containerRef.value as HTMLElement
 			if (parentElement) {
-				resizeObserver.observe(parentElement.parentNode?.parentNode as HTMLElement)
+				resizeObserver.observe(parentElement.parentNode as HTMLElement)
 			}
 		}
 	})
@@ -226,32 +173,14 @@
 		}
 		resizeObserver = null
 		containerRef.value = null
-		xAxisData = null
 		seriesData = null
 	})
 </script>
 <template>
-	<div class="w-full h-full mt-2 border-b border-[--border-color] py-4 min-h-[350px] flex flex-col justify-between" ref="containerRef" :style="{ width: width > 0 ? width + 'px' : 'auto' }">
-		<div class="flex items-center justify-between mb-2">
-			<h3 class="text-sm flex items-center">
-				<b class="text-base">杠杆多空比</b>
-			</h3>
-			<el-radio-group v-model="period" :disabled="disabled" size="small" v-click-sound>
-				<el-radio-button value="5m">5分钟</el-radio-button>
-				<el-radio-button value="1H">1小时</el-radio-button>
-				<el-radio-button value="1D">1天</el-radio-button>
-			</el-radio-group>
+	<div class="w-full h-full flex flex-col justify-between" ref="containerRef">
+		<div class="container w-full h-full flex-1" v-show="!loading && !error" :style="{ width: width > 0 ? width + 'px' : '100%', height: height > 0 ? height + 'px' : '100%' }">
+			<div class="chart w-full h-full" ref="chart"></div>
 		</div>
-
-		<div class="container w-full h-full flex-1" v-show="!loading && !error">
-			<div class="chart w-full h-[285px]" ref="chart"></div>
-		</div>
-		<el-skeleton :rows="7" animated v-if="loading && !error" />
-		<Error :content="error" v-if="!loading && error" class="flex-1">
-			<template #default>
-				<el-button type="primary" @click.stop="fetchData(Period.M5, true)">点击刷新</el-button>
-			</template>
-		</Error>
 	</div>
 </template>
 <style lang="less" scoped></style>
--- components/datas/MarketCategories.vue
+++ components/datas/MarketCategories.vue
@@ -1,24 +1,58 @@
 <script setup lang="ts">
-	const categories = ref([{}, {}, {}, {},{}, {}, {}, {}])
+	import LineChart from '../common/LineChart.vue'
+
+	const categories = ref([{}, {}, {}, {}, {}, {}, {}, {}])
+	const loading = ref(true)
+
+	onMounted(() => {
+		setTimeout(() => {
+			loading.value = false
+		}, 0)
+	})
 </script>
 <template>
 	<div class="market-category px-4 py-2">
 		<h3 class="pb-3 flex justify-between items-center">
 			热门板块 <el-icon><ElIconArrowRight /></el-icon>
 		</h3>
-        <div class="w-full overflow-x-scroll scrollbar-hide">
-		<ul class="flex w-max">
-			<template v-for="item in categories">
-				<li class="p-2 rounded-md bg-[--transparent02] w-[calc(var(--body-width)/2)] mr-2">
-					<h4 class="text-sm font-bold">Liquid Staking</h4>
-					<div class="text-sm flex flex-col py-3">
-						<b class="text-green text-base font-bold">+1.70%</b>
-						<span class="text-xs text-grey">24小时涨跌</span>
-					</div>
-					<div class="text-xs flex"><span>RPL</span><span class="text-green">+8.68%</span></div>
-				</li>
-			</template>
-		</ul>
-        </div>
+		<div class="w-full overflow-x-scroll scrollbar-hide">
+			<ul class="flex w-max" v-if="!loading">
+				<template v-for="item in categories">
+					<li class="p-3 rounded-md overflow-hidden bg-[--transparent02] glass-1 w-[calc(var(--body-width)/2)] mr-2 relative">
+						<h4 class="text-sm font-bold">Liquid Staking</h4>
+						<div class="text-sm flex flex-col py-3">
+							<b class="text-green text-base font-bold">+1.70%</b>
+							<span class="text-xs text-grey">24小时涨跌</span>
+						</div>
+						<div class="text-xs flex"><span>RPL</span><span class="text-green px-1">+8.68%</span></div>
+						<LineChart symbol="BTC-USDT" class="absolute right-0 top-0 w-2/5 h-1/2 translate-y-2/3 mx-3" />
+					</li>
+				</template>
+			</ul>
+			<ul class="flex w-max" v-else>
+				<template v-for="item in [{}, {}]">
+					<li class="p-3 rounded-md overflow-hidden bg-[--transparent02] w-[calc(var(--body-width)/2)] mr-2 relative">
+						<el-skeleton :rows="0" animated>
+							<template #template>
+								<el-skeleton-item variant="p" style="width: 60%; height: 12px" />
+							</template>
+						</el-skeleton>
+						<div class="text-sm flex flex-col py-3">
+							<el-skeleton :rows="0" animated class="flex flex-col justify-center gap-2">
+								<template #template>
+									<el-skeleton-item variant="p" style="width: 40%; height: 5px" />
+									<el-skeleton-item variant="p" style="width: 30%; height: 5px" />
+								</template>
+							</el-skeleton>
+						</div>
+						<el-skeleton :rows="0" animated>
+							<template #template>
+								<el-skeleton-item variant="p" style="width: 50%; height: 5px" />
+							</template>
+						</el-skeleton>
+					</li>
+				</template>
+			</ul>
+		</div>
 	</div>
 </template>
