9ced53e01320b331b869b35233f2f6dfaab67d1e
deepkline <dangfm@qq.com>
2025-06-15 12:05:58 +0800

    优化市场情绪呼吸灯

--- components/datas/charts/HalfCircleRainbow.vue
+++ components/datas/charts/HalfCircleRainbow.vue
@@ -15,6 +15,11 @@
 					<!-- 绿 -->
 				</linearGradient>
 
+                <linearGradient id="innerLine" gradientUnits="userSpaceOnUse" x1="40" y1="100" x2="180" y2="100">
+					<stop offset="0%" stop-color="#ffffff" stop-opacity="0.3"/>
+					<stop offset="100%" stop-color="#ffffff" stop-opacity="0.0"/>
+				</linearGradient>
+
 				<!-- 定义从圆心发散的径向渐变 -->
 				<radialGradient id="grad1"  cx="50%" cy="50%" r="50%">
 					<stop offset="0%" :stop-color="red" stop-opacity="1"/>
@@ -35,6 +40,7 @@
 
 			<!-- 半圆渐变弧 -->
 			<path :d="describeArc(100, 100, radius, -90, 90)" fill="none" stroke="url(#rainbowGradient)" stroke-width="10" />
+            <path :d="describeArc(100, 100, radius-10, -90, 90)" fill="none" stroke="url(#innerLine)" stroke-width="1" />
 
 			<!-- 扇形区域 -->
 			<path :d="generateSectorPath(100, 100, radius, -90, value - 90)" fill="url(#grad1)" fill-opacity="0.1" />
